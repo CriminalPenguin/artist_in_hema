856db3f190dcbab034608fea6a4d816cef58f9be
2-jun9 <jungu269@gmail.com>
2022-04-03 00:13:09 +0900

    1페이지 2파트 디자인 작업중

--- hema-app/src/App.js
+++ hema-app/src/App.js
@@ -1,48 +1,82 @@
 import './App.css';
+import note from './img/note.svg';
 
 function App() {
   return (
     <div className="App">
-      <div className="Div1">
-        <Div1></Div1>
-      </div>
-      <div className="Div2">
-        <Div2></Div2>
-      </div>
-      <div className="Div3">
-        <Div3></Div3>
-      </div>
+      <Div1></Div1>
+      <Div2></Div2>
+      <Div3></Div3>
     </div>
   );
-}
+} // 전체 페이지 렌더링 함수
 
 function Div1() {
   return (
-    <div className="Imagebox">
-      <div className="Ssentence">
-        <div className="Logo">(로고) HEMA</div>
-        <div className="Small1">Music,</div>
-        <div className="Small2">Connect Everyone</div>
+    <div className="Div1">
+      <div className="Imagebox">
+        <div className="Ssentence">
+          <div className="Logo">(로고) HEMA</div>
+          <div className="Small1">Music,</div>
+          <div className="Small2">Connect Everyone</div>
+        </div>
+        <div className="Bsentence">
+          <div className="Big1">Eat, drink, listen and play.</div>
+          <div className="Big2">Find your joy with amazing people</div>
+        </div>
+        <div className="Logobox">
+          <a href="https://www.instagram.com/hema_1991_/?hl=ko">
+            <img src={require("./img/instagram.png")}></img>
+          </a>
+          <a href="https://www.facebook.com/groups/gotbeer">
+            <img src={require("./img/facebook.png")} href=""></img>
+          </a>
+        </div>
+        <img className="Mainimage" src={require("./img/Main.jpg")}></img>
       </div>
-      <div className="Bsentence">
-        <div className="Big1">Eat, drink, listen and play.</div>
-        <div className="Big2">Find your joy with amazing people</div>
-      </div>
-      <img className="Mainimage" src={require("./img/Main.jpg")}></img>
     </div>
   );
-}
+} //파트1만 렌더링
 
 function Div2() {
   return (
-    <div>History역사</div>
+    <div className="Div2">
+      <div className="PageTitle">
+        <div className="Title">Timeline</div>
+      </div>
+      <div className="Graph">
+        <div className="TopLine"></div>
+        <div className="Lines">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="Clef">
+          <img src={require("./img/treble-clef.png")}></img>
+        </div>
+        <div className="Note">
+          <div className="ThisNote">
+            <img src={note}></img>
+            <div className="About">1991. 동아리 창립</div>
+          </div>
+          <div className="NoteLine"></div>
+        </div>
+      </div>
+      <div>
+
+      </div>
+    </div>
   );
-}
+} //파트2만 렌더링
 
 function Div3() {
   return (
-    <div>Random Image</div>
+    <div className="Div3">
+      <div>Random Image</div>
+    </div>
   );
-}
+} //파트3만 렌더링
 
 export default App;
