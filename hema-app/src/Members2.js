61312b2b3e719eed302b38dfb13d915be0fd8ffe
2-jun9 <jungu269@gmail.com>
2022-04-15 00:34:20 +0900

    2페이지 리디자인

--- hema-app/src/Members2.js
+++ hema-app/src/Members2.js
@@ -20,6 +20,7 @@ const 가입년도2=['06','07','08','09','10',
               '16','17','18','19','20']
 
 function Members() {
+
   return(
     <div>
       {/* <Div1></Div1> */}
@@ -50,13 +51,68 @@ function Div2() {
     <div className="Main">
       {divNum.map(function(a){
         return(
-          <div className="Display">
-            
-          </div>
+          <Show a={a}></Show>
         )
       })}  
     </div>
   );
 } // 실제 표시되는 부분(사진, 내용)
+
+function Show(props) {
+  var data = dummy.members.filter(function(element){
+    return element.join === props.a;
+  }) // 받아온 가입년도 정보를 기반으로 가입년도별 데이터 추출
+  
+  let [num, num변경] = useState(0);
+  var 선택자 = data[num]
+
+  return (
+    <div className="Display">
+            <div className="Part1"><p>Playing Now {props.a}</p></div>
+            <div className="Part2">
+              <div className="Circle">
+                <div className="Photo">
+                  <img src={require("./sample/"+ 선택자.id + ".jpeg")}></img>
+                </div>
+              </div>
+            </div>
+            <div className="Part3">
+              <p id="name">{선택자.ename}</p>
+              <div id="session">
+                {선택자.session.map(function(b){
+                  return(
+                    <div>
+                      {b}&nbsp;&nbsp;
+                    </div>
+                  )
+                })}
+              </div>
+              <div id="band">
+                {선택자.band.map(function(b){
+                  return(
+                    <div>
+                      {b}&nbsp;&nbsp;
+                    </div>
+                  )
+                })}
+              </div>
+            </div>
+            <div className="Part4">
+              <div className="Many">
+                <div>0:00</div>
+                <div>{data.length}:00</div>
+              </div>
+              <div className="Bar"></div>
+              <div className="Now"><div></div></div>
+            </div>
+            <div className="Part5">
+              <div onClick={()=>num변경(num-1)}></div>
+              <div id="play"></div>
+              <div onClick={()=>num변경(num+1)}></div>
+            </div>
+          </div>
+      
+  )
+} // 사진 불러오기
 
 export default Members;
