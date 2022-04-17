849cae60bb619a7dcaa99bbb112bbf4b2778dd0b
2-jun9 <jungu269@gmail.com>
2022-04-17 19:54:58 +0900

    등록 페이지 기능 구현중, 정보 저장

--- hema-app/src/Register.js
+++ hema-app/src/Register.js
@@ -1,6 +1,12 @@
 import './Register.scss';
 import React, { useState } from 'react';
 
+import dummy from "./data/data.json"
+
+
+const group = [];
+const group2 = [];
+
 function Register() {
   let [등록정보, 등록정보변경] = useState({
     id : "",
@@ -9,8 +15,8 @@ function Register() {
     classe : "",
     ename : "",
     phone : "",
-    session : [],
-    band : [],
+    session : "",
+    band : "",
     insta : ""
   })
 
@@ -23,6 +29,22 @@ function Register() {
       [name]: value // name 키를 가진 값을 value 로 설정
     });
   };
+
+  const onChangeCheck = (e) => {
+    const { checked, id } = e.target; // 우선 e.target 에서 name 과 value 를 추출
+    if(checked === true){
+      group.push(id);
+    }
+    else if( checked === false){
+      group.pop(id);
+    }
+  };
+
+  function sendData () {
+    등록정보.session = group;
+    등록정보.band = group2;
+    console.log(등록정보);
+  }
 
   return (
     <div className="Page2">
@@ -45,22 +67,22 @@ function Register() {
         <div className="P4">
           <div className="Font">세션선택</div>
           <div className="Sess">
-            <label><input type="checkbox" id="s1"/>Vocal</label>
-            <label><input type="checkbox" id="s2"/>Guitar</label>
-            <label><input type="checkbox" id="s3"/>Bass</label>
-            <label><input type="checkbox" id="s4"/>Drum</label>
-            <label><input type="checkbox" id="s5"/>Keyboard</label>
-            <label><input type="checkbox" id="s6"/>Synthesizer</label> 
+            <label><input type="checkbox" id="Vocal" name="session" onChange={onChangeCheck}/>Vocal</label>
+            <label><input type="checkbox" id="Guitar" name="session" onChange={onChangeCheck}/>Guitar</label>
+            <label><input type="checkbox" id="Bass" name="session" onChange={onChangeCheck}/>Bass</label>
+            <label><input type="checkbox" id="Drum" name="session" onChange={onChangeCheck}/>Drum</label>
+            <label><input type="checkbox" id="Keyboard" name="session" onChange={onChangeCheck}/>Keyboard</label>
+            <label><input type="checkbox" id="Synthesizer" name="session" onChange={onChangeCheck}/>Synthesizer</label> 
           </div>
         </div>
         <div className="P5">
           <div className="Font">참여했던 밴드</div>
           <div className="band">
-            <div className="input-container"><input type="text" id="b1"/><label>1</label></div>
-            <div className="input-container"><input type="text" id="b2"/><label>2</label></div>
-            <div className="input-container"><input type="text" id="b3"/><label>3</label></div>
-            <div className="input-container"><input type="text" id="b4"/><label>4</label></div>
-            <div className="input-container"><input type="text" id="b5"/><label>5</label></div>
+            <div className="input-container"><input type="text" id="b1" name="band" value={band} /><label>1</label></div>
+            <div className="input-container"><input type="text" id="b2" name="band" value={band} /><label>2</label></div>
+            <div className="input-container"><input type="text" id="b3" name="band" value={band} /><label>3</label></div>
+            <div className="input-container"><input type="text" id="b4" name="band" value={band} /><label>4</label></div>
+            <div className="input-container"><input type="text" id="b5" name="band" value={band} /><label>5</label></div>
           </div>
         </div>
         <div className="P6">
@@ -70,7 +92,7 @@ function Register() {
             <input className="form-field" type="text" placeholder="인스타그램 ID" name="insta" value={insta} onChange={onChange}/>
           </div>
         </div>
-        <div className="btn"><button class="button-77" role="button">등록하기</button></div>
+        <div className="btn"><button class="button-77" role="button" onClick={sendData}>등록하기</button></div>
         <div className="Empty"></div>
       </div>
     </div>
