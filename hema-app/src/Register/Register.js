741f4fa3a2d220d3f3d7c13d0a47700f1b517b3d
2-jun9 <jungu269@gmail.com>
2022-04-25 22:43:38 +0900

    멤버 등록 Policy 추가

--- hema-app/src/Register/Register.js
+++ hema-app/src/Register/Register.js
@@ -9,7 +9,6 @@ var auto = 1;
 
 function Register() {
   
-
   let [등록정보, 등록정보변경] = useState({
     id : "",
     join : "",
@@ -74,54 +73,76 @@ function Register() {
   }
 
   return (
-    <div className="Page2">
-      <div className="Nemo">
-        <div className="Empty"></div>
-        <div className="P1">
-          <div className="input-container"><input type="number" placeholder="1991" name="join" value={join} onChange={onChange}/><label>가입년도</label></div>
-          <div className="input-container"><input type="number" placeholder="91" name="classe" value={classe} onChange={onChange}/><label>학번</label></div>
-        </div>
-        <div className="P2">
-          <div className="input-container"><input type="text" placeholder="이준구" name="name" value={name} onChange={onChange}/><label>이름</label></div>
-          <div className="input-container"><input type="text" placeholder="JUNGU LEE" name="ename" value={ename} onChange={onChange}/><label>영문이름</label></div>
-        </div>
-        <div className="P3">
-        <div className="input-container"><input type="number" placeholder="01041391978" name="phone" value={phone} onChange={onChange}/><label>전화번호</label></div>
-        </div>
-        <div className="P3">
-        <label className="La" for="chooseFile">📸사진을 골라주세요📸</label><input type="file" id="chooseFile" name="chooseFile" accept="image/*"></input>
-        </div>
-        <div className="P4">
-          <div className="Font">세션선택</div>
-          <div className="Sess">
-            <label><input type="checkbox" id="Vocal" name="session" onChange={onChangeCheck}/>Vocal</label>
-            <label><input type="checkbox" id="Guitar" name="session" onChange={onChangeCheck}/>Guitar</label>
-            <label><input type="checkbox" id="Bass" name="session" onChange={onChangeCheck}/>Bass</label>
-            <label><input type="checkbox" id="Drum" name="session" onChange={onChangeCheck}/>Drum</label>
-            <label><input type="checkbox" id="Keyboard" name="session" onChange={onChangeCheck}/>Keyboard</label>
-            <label><input type="checkbox" id="Synthesizer" name="session" onChange={onChangeCheck}/>Synthesizer</label> 
+    <div className='This'>
+      <Policy></Policy>
+      <div className="Page2">
+        <div className="Nemo">
+          <div className="Empty"></div>
+          <div className="P1">
+            <div className="input-container"><input type="number" placeholder="1991" name="join" value={join} onChange={onChange}/><label>가입년도</label></div>
+            <div className="input-container"><input type="number" placeholder="91" name="classe" value={classe} onChange={onChange}/><label>학번</label></div>
           </div>
-        </div>
-        <div className="P5">
-          <div className="Font">참여했던 밴드</div>
-          <div className="band">
-            <div className="input-container"><input type="text" name="b1" value={b1} onChange={onChangeBand}/><label>1</label></div>
-            <div className="input-container"><input type="text" name="b2" value={b2} onChange={onChangeBand}/><label>2</label></div>
-            <div className="input-container"><input type="text" name="b3" value={b3} onChange={onChangeBand}/><label>3</label></div>
-            <div className="input-container"><input type="text" name="b4" value={b4} onChange={onChangeBand}/><label>4</label></div>
-            <div className="input-container"><input type="text" name="b5" value={b5} onChange={onChangeBand}/><label>5</label></div>
+          <div className="P2">
+            <div className="input-container"><input type="text" placeholder="이준구" name="name" value={name} onChange={onChange}/><label>이름</label></div>
+            <div className="input-container"><input type="text" placeholder="JUNGU LEE" name="ename" value={ename} onChange={onChange}/><label>영문이름</label></div>
           </div>
-        </div>
-        <div className="P6">
-          <div>인스타그램</div>
-          <div className="form-group">
-            <span>https://www.instagram.com/</span>
-            <input className="form-field" type="text" placeholder="인스타그램 ID" name="insta" value={insta} onChange={onChange}/>
+          <div className="P3">
+          <div className="input-container"><input type="number" placeholder="01041391978" name="phone" value={phone} onChange={onChange}/><label>전화번호</label></div>
           </div>
+          <div className="P3">
+          <label className="La" for="chooseFile">📸사진을 골라주세요📸</label><input type="file" id="chooseFile" name="chooseFile" accept="image/*"></input>
+          </div>
+          <div className="P4">
+            <div className="Font">세션선택</div>
+            <div className="Sess">
+              <label><input type="checkbox" id="Vocal" name="session" onChange={onChangeCheck}/>Vocal</label>
+              <label><input type="checkbox" id="Guitar" name="session" onChange={onChangeCheck}/>Guitar</label>
+              <label><input type="checkbox" id="Bass" name="session" onChange={onChangeCheck}/>Bass</label>
+              <label><input type="checkbox" id="Drum" name="session" onChange={onChangeCheck}/>Drum</label>
+              <label><input type="checkbox" id="Keyboard" name="session" onChange={onChangeCheck}/>Keyboard</label>
+              <label><input type="checkbox" id="Synthesizer" name="session" onChange={onChangeCheck}/>Synthesizer</label> 
+            </div>
+          </div>
+          <div className="P5">
+            <div className="Font">참여했던 밴드</div>
+            <div className="band">
+              <div className="input-container"><input type="text" name="b1" value={b1} onChange={onChangeBand}/><label>1</label></div>
+              <div className="input-container"><input type="text" name="b2" value={b2} onChange={onChangeBand}/><label>2</label></div>
+              <div className="input-container"><input type="text" name="b3" value={b3} onChange={onChangeBand}/><label>3</label></div>
+              <div className="input-container"><input type="text" name="b4" value={b4} onChange={onChangeBand}/><label>4</label></div>
+              <div className="input-container"><input type="text" name="b5" value={b5} onChange={onChangeBand}/><label>5</label></div>
+            </div>
+          </div>
+          <div className="P6">
+            <div>인스타그램</div>
+            <div className="form-group">
+              <span>https://www.instagram.com/</span>
+              <input className="form-field" type="text" placeholder="인스타그램 ID" name="insta" value={insta} onChange={onChange}/>
+            </div>
+          </div>
+          <div className="btn"><button class="button-77" role="button" onClick={sendData}>등록하기</button></div>
+          <div className="Empty"></div>
         </div>
-        <div className="btn"><button class="button-77" role="button" onClick={sendData}>등록하기</button></div>
-        <div className="Empty"></div>
       </div>
+    </div>
+  );
+}
+
+function Policy() {
+  return (
+    <div className='Page2'>
+      <h1>📋 The Policy of this site 📋</h1>
+      <h3>이 사이트는 한양대학교 ERICA 밴드 동아리 HEMA의 졸업생 및 재학생 멤버 정보를 관리하기 위한 사이트입니다</h3>
+      <h3>📍 등록 기준</h3>
+      <p>2년 이상 동아리 회원으로 활동하였으며 졸업 이후에도 동아리 행사에 참여하였거나 회원들과의 모임에 참여하는 자</p>
+      <h3>📍 등록 방법</h3>
+      <p>하단에 있는 양식에 따라 정보를 기입하여 제출하여 주시면 간단한 확인 이후에 멤버 정보를 등록해드립니다</p>
+      <h3>📍 정보 관리</h3>
+      <p>전화 번호 등 개인신상과 관련된 정보는 차후 동아리 관련 연락을 위한 목적으로만 사용되며 사이트에서는 노출되지 않습니다</p>
+      <p>🛠🛠🛠</p>
+      <p>문의 및 버그 신고 👉 12 이준구 📱 010-4139-1978</p>
+      <p>개발 및 운영 총괄 👉 12 이준구 <a href="https://jun9.oopy.io/">🚀</a></p>
+      <p><a href="https://github.com/2-jun9/artist_in_hema">소스코드 열람 👀</a></p>
     </div>
   );
 }
